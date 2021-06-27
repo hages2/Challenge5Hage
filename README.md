426f19ebcd2b6c898c3452cc7b03845046ca24da
Stephanie Hage <stephaniehage23@gmail.com>
2021-06-26 21:57:37 -0400

    saving to local storage

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Challenge5Hage
+#Work Schedule
+
+Purpose
+A website uses a daily planner to create a schedule for the work day.
+
+Built With
+HTML
+CSS
+Javascript
+jquery
+moment.js
+Bootstrap
+Iconic
+
+Website
+https://hages2.github.io/Challenge5Hage/
+
+Contribution
+Made with ❤️ by Stephanie
--- assets/js/script.js
+++ assets/js/script.js
@@ -37,10 +37,40 @@ var timeBlocks = [{
 ];
 //create an array with 8 time blocks from 9 to 5
 
+
+var saveButtonClickHandler = function(event){
+    //Get the ID of the Button that was clicked
+    //Remove -button so you can lookup WHICH timeblock to update in
+    //you arrary and local storage based off the index
+    var clickedElementID = event.currentTarget.id;
+    var elementID = clickedElementID.replace("-button","");
+    
+    //Get the value types in to the text area in the timeblock that was clicked
+    var typedItem = $("#"+elementID+"-text");
+    
+    //Replace the item in the array
+    timeBlocks[elementID].item = typedItem.val();
+
+    //Write the array to local storage
+    localStorage.setItem("schedule", JSON.stringify(timeBlocks));
+
+};
+
 var renderTimeBlocks = function () {
+
+    //real quick put datte at top of page
+    $("#currentDay").text(moment().format('MM/DD/YYYY'))
+
     //this function will create 8 li's and render them into the ul
     //get refernce to ul from html so that we can append child lis
     var timeBlockUl = $("#time-block-list");
+    
+    //before we render the timeBlocks array, check local storage!! and see if there 
+    //are already shceduled items that need to be rendered
+    scheduledItems = JSON.parse(localStorage.getItem("schedule"));
+    if (scheduledItems){
+        timeBlocks = scheduledItems
+    }
 
     for (var i = 0; i < timeBlocks.length; i++) {
 
@@ -54,12 +84,12 @@ var renderTimeBlocks = function () {
         timeBlocksRow.append(scheduleTimeDiv);
 
         var scheduleTextDiv = $("<div>").addClass("schedule col-md-8");
-        var scheduleTextInput = $("<textarea>").addClass("input-text-area");
+        var scheduleTextInput = $("<textarea>").addClass("input-text-area").attr("id",i+'-text').val(timeBlocks[i].item);
         scheduleTextDiv.append(scheduleTextInput);
         timeBlocksRow.append(scheduleTextDiv);
 
         var saveButtonDiv = $("<div>").addClass("save");
-        var saveButtonEl = $("<button>").addClass("saveBtn col-md-2");
+        var saveButtonEl = $("<button>").addClass("saveBtn col-md-2").attr("id",i+'-button').bind( "click",saveButtonClickHandler);
         var saveIconSpan = $("<span>").addClass("oi oi-lock-locked iconic-vertical-align-center");
         saveButtonEl.append(saveIconSpan);
         saveButtonDiv.append(saveButtonEl);
@@ -70,9 +100,7 @@ var renderTimeBlocks = function () {
         var currentBlockTime = timeBlocks[i].time;
         //convert string to moment object
         var currentBlockTimeMoment = moment(currentBlockTime, "HH").hour();
-        var now = moment().set("hour", 13).hour()
-        console.log(now);
-        console.log(currentBlockTimeMoment)
+        var now = moment().hour()
 
         if (now>currentBlockTimeMoment) {
             timeBlocksLi.addClass("list-group-item-secondary");
@@ -87,6 +115,11 @@ var renderTimeBlocks = function () {
 }
 
 renderTimeBlocks();
+
+
+
+//save when we click save button and will need to give each item an id 
+
 
 
 
@@ -131,3 +164,5 @@ renderTimeBlocks();
 //  //save schedule item to local storage
 //  var saveItems = function () {
 //     localStorage.setItem("items", JSON.stringify(items));
+
+
